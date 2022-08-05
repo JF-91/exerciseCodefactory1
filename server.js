6e81dbd49a25acc1e993d913bcb7573f66204483
JF-91 <77854220+JF-91@users.noreply.github.com>
2022-08-05 09:11:16 +0200

    commit 4, activities dont run

--- server.js
+++ server.js
@@ -9,41 +9,41 @@ const path = require('path')
 app.get('/', (req, res)=>{
 
     
-        res.sendFile(path.resolve(__dirname, './public/index.html'))
+        res.sendFile(path.join(__dirname, './public/index.html'))
     
 })
 app.get('/song2', (req, res)=>{
 
   
-        res.sendFile(path.resolve(__dirname, './public/statics/song2.html'))
+        res.sendFile(path.join(__dirname, './public/statics/song2.html'))
     
 })
 
 app.get('/song3', (req, res)=>{
 
     
-        res.sendFile(path.resolve(__dirname, './public/statics/song3.html'))
+        res.sendFile(path.join(__dirname, './public/statics/song3.html'))
     
 })
 
 app.get('/song4', (req, res)=>{
 
     
-        res.sendFile(path.resolve(__dirname,'./public/statics/song4.html'))
+        res.sendFile(path.join(__dirname,'./public/statics/song4.html'))
     
 })
 
 app.get('/song5', (req, res)=>{
 
     
-        res.sendFile(path.resolve(__dirname, './public/statics/song5.html'))
+        res.sendFile(path.join(__dirname, './public/statics/song5.html'))
     
 })
 
 app.get('/activities', (req, res)=>{
 
    
-        res.sendFile(path.resolve(__dirname, './public/statics/Vienna-Activities.html'))
+        res.sendFile(path.join(__dirname, '.public/statics/activities.html'))
 
 })
 
